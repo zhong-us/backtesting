07e546b84aac160de9589a38b6519a1b38d63c9e
ZZ <zhong.us@outlook.com>
2023-01-27 21:08:39 -0500

    initial commit

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+'# \"In the short run,'
+'# the market is a voting machine'
+'# but in the long run,'
+'# it is a weighing machine.\"'
+'## - Warren Buffett'
+'#### This is an app made to \'hack\' the voting machine.'
+'#### It will test and visualize one* momentum trading strategy.'
+st.caption('*More on the way.')
+'#### Momentum traders look to take advantage of upward trends in a stock\'s price and they believe the trend will continue to head in the same direction for a period of time.'
+'#### In the next page, you will be able to choose your own parameters for determining the strength of a trend and different backtesting period.'
+st.caption('Play safe with your own money.')
+st.caption('&larr; Check the sidebar!')
---
+++ pages/99 About.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+'## \"Be fearful when others are greedy and'
+'## to be greedy only when others are fearful\"'
+'### - Warren Buffett'
+'Momentum trading is exactly the opposite to one of the most known quote in investment world.'
+'While personally I believe momentum trading has it\'s place and can shine if excuted with discipline,'
+'It is no doubt much more volatile than traditional buy-and-hold strategy.'
+'Analysis of price history just shows past performance and is no guarantee of future results.'
+'And it is only meaningful for short-term strategy.'
+''
+'Future work include adding more custom options for fine tuning strategies and visualizations, adding more different strategies with more indicators, maybe adding larger pool of stocks/ETFs to choose from.'
+''
+'Special thanks to [yfinance](https://pypi.org/project/yfinance/) for real time market data.'
+'The information contained herein does not constitute investment advice and is not warranted to be accurate, complete, or timely.'
+'I am not responsible for any damages or losses arising from any use of this information.'
